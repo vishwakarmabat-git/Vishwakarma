85b081d6439cf70dc5e22e51a171d86584a19b15
avi1804 <raoh0605@gmail.com>
2026-08-23 15:53:04 +0530

    fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
